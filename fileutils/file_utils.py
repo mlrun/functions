10e64e47f27473d4c59960e28b46ea0de453a2f3
yasha <yashab@iguazio.com>
2020-01-09 18:32:38 +0000

    add arc_to_parquet

--- fileutils/file_utils.py
+++ fileutils/file_utils.py
@@ -1,24 +1,108 @@
+# Copyright 2019 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
 import zipfile
 import json
 from tempfile import mktemp
+from pathlib import Path
 
+import pandas as pd
+import pyarrow.parquet as pq
+import pyarrow as pa
 
-def open_archive(context, 
-                 target_dir='content',
-                 archive_url=''):
-    """Open a file/object archive into a target directory"""
-        
+from mlrun.execution import MLClientCtx
+from typing import IO, AnyStr, Union, List
+
+def open_archive(context: MLClientCtx, 
+                 target_path: str = '',
+                 key: str = 'content',
+                 archive_url: = ''):
+    """Open a file/object archive into a target directory.
+
+    The file contents will be available within target_dir, and
+    and target_dir will be logged as an artifact under the key
+    specified (defaults to 'content').
+
+    :param context:      function context
+    :param target_dir:   destination for file artifact
+    :param archive_url:  source archive url
+    """
     # Define locations
-    os.makedirs(target_dir, exist_ok=True)
-    context.logger.info('Verified directories')
+    os.makedirs(target_path, exist_ok=True)
+    context.logger.info('verified directories')
     
     # Extract dataset from zip
-    context.logger.info('Extracting zip')
+    context.logger.info('extracting zip')
     zip_ref = zipfile.ZipFile(archive_url, 'r')
     zip_ref.extractall(target_dir)
     zip_ref.close()
     
     context.logger.info(f'extracted archive to {target_dir}')
-    context.log_artifact('content', target_path=target_dir)
+    context.log_artifact(key, target_path=target_dir)
     
+import os
+
+
+def arc_to_parquet(
+    context: MLClientCtx,
+    archive_url: Union[str, Path, IO[AnyStr]],
+    header: Union[None, List[str]] = None,
+    target_path: str = "",
+    name: str = "",
+    chunksize: int = 10_000,
+    log_data: bool = True,
+    key: str = 'raw_data'
+) -> None:
+    """Open a file/object archive and save as a parquet file.
+    
+    :param context:     function context
+    :param archive_url: any valid string path consistent with the path variable
+                        of pandas.read_csv. ncluding strings as file paths, as urls, 
+                        pathlib.Path objects, etc...
+    :param header:      column names
+    :param target_path: destination folder of table
+    :param name:        name file to be saved locally, also
+    :param chunksize:   (0) row size retrieved per iteration
+    :param log_data:    (True) if True, log the data so that it is available
+                        at the next step
+    :param key:         key in artifact store (when log_data=True)
+    """
+    os.makedirs(target_path, exist_ok=True)
+
+    if not name.endswith(".parquet"):
+        name += ".parquet"
+
+    dest_path = os.path.join(target_path, name)
+
+    if not os.path.isfile(dest_path):
+        context.logger.info("destination file does not exist, downloading")
+        pqwriter = None
+        for i, df in enumerate(
+            pd.read_csv(archive_url, chunksize=chunksize, names=header)
+        ):
+            table = pa.Table.from_pandas(df)
+            if i == 0:
+                pqwriter = pq.ParquetWriter(dest_path, table.schema)
+            pqwriter.write_table(table)
+
+        if pqwriter:
+            pqwriter.close()
+
+        context.logger.info(f"saved table to {dest_path}")
+    else:
+        context.logger.info("destination file exists")
+
+    if log_data:
+        context.logger.info("logging data to context")
+        context.log_artifact(key, target_path=dest_path)

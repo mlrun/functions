94a4aab5417c8979c32226656885a18cdb26cc28
yasha <yashab@iguazio.com>
2020-01-21 15:51:05 +0000

    arc-to-parq fixes

--- fileutils/arc_to_parquet/arc_to_parquet.py
+++ fileutils/arc_to_parquet/arc_to_parquet.py
@@ -1,8 +1,23 @@
+# Copyright 2018 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
+import json
 from pathlib import Path
 import pandas as pd
 import pyarrow.parquet as pq
 import pyarrow as pa
+from pickle import dump, load
 
 from mlrun.execution import MLClientCtx
 from typing import IO, AnyStr, Union, List, Optional
@@ -11,10 +26,10 @@ from typing import IO, AnyStr, Union, List, Optional
 def arc_to_parquet(
     context: MLClientCtx,
     archive_url: Union[str, Path, IO[AnyStr]],
-    header: Union[int, List[str], None] = 0,
+    header: Optional[List[str]] = None,
     target_path: str = "",
     name: str = "",
-    chunksize: int = 10_240,
+    chunksize: int = 10_000,
     log_data: bool = True,
     add_uid: bool = False,
     key: str = "raw_data",
@@ -29,33 +44,19 @@ def arc_to_parquet(
     :param target_path: destination folder of table
     :param name:        name file to be saved locally, also
     :param chunksize:   (0) row size retrieved per iteration
-    :param log_data:    (True) if True, log the data so that it is available
-                        at the next step
-    :param add_uid:     (False) add the metadata uid to the target_path so that 
-                        runs can be identified
     :param key:         key in artifact store (when log_data=True)
     """
     if not name.endswith(".parquet"):
         name += ".parquet"
 
-    if not add_uid:
-        uid = ""
-    else:
-        uid = context.uid
-
-    dest_path = os.path.join(target_path, uid, name)
-    os.makedirs(os.path.join(target_path, uid), exist_ok=True)
-    
+    dest_path = os.path.join(target_path, name)
+    os.makedirs(os.path.join(target_path), exist_ok=True)
     if not os.path.isfile(dest_path):
         context.logger.info("destination file does not exist, downloading")
         pqwriter = None
-        for i, df in enumerate(
-            pd.read_csv(archive_url, chunksize=chunksize, names=header)
-        ):
+        for i, df in enumerate(pd.read_csv(archive_url, chunksize=chunksize, names=header)):
             table = pa.Table.from_pandas(df)
             if i == 0:
-                header = list(df)
-                header = [x.replace(' ', '_') for x in header]
                 pqwriter = pq.ParquetWriter(dest_path, table.schema)
             pqwriter.write_table(table)
 
@@ -66,10 +67,8 @@ def arc_to_parquet(
     else:
         context.logger.info("destination file already exists")
 
-    if log_data:
-        context.log_artifact(key, target_path=dest_path)
-        # log header
-        filepath = path.join(target_path, 'header.json')
-        json.dump(header, open(filepath, 'w'))
-        context.log_artifact('header', target_path=filepath)
-            
+    context.log_artifact(key, target_path=dest_path)
+    # log header
+    filepath = os.path.join(target_path, 'header.pkl')
+    dump(header, open(filepath, 'wb'))
+    context.log_artifact('header', target_path=filepath)

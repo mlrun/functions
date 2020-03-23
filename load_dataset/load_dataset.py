4d06cb976ca908a5cfe269fed774bda211fcd605
Yaron Haviv <yaronh@iguaz.io>
2020-03-23 21:32:24 +0200

    update describe and load_dataset

--- load_dataset/load_dataset.py
+++ load_dataset/load_dataset.py
@@ -11,28 +11,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
-import json
-from pathlib import Path
+
 import numpy as np
 import pandas as pd
-from cloudpickle import dump, load
-
 from mlrun.execution import MLClientCtx
-from mlrun.datastore import DataItem
 
-from typing import IO, AnyStr, Union, List, Optional
 
 def get_toy_data(
-    context: MLClientCtx,
-    dataset: str,
-    params: dict = {},
-    format: str = "parquet"
+        context: MLClientCtx,
+        dataset: str,
+        format: str = 'pq',
+        name: str = '',
+        params: dict = {}
 ) -> None:
     """Loads a scikit-learn toy dataset for classification or regression
-    
+
     The following datasets are available ('name' : desription):
-    
+
         'boston'          : boston house-prices dataset (regression)
         'iris'            : iris dataset (classification)
         'diabetes'        : diabetes dataset (regression)
@@ -40,51 +35,43 @@ def get_toy_data(
         'linnerud'        : linnerud dataset (multivariate regression)
         'wine'            : wine dataset (classification)
         'breast_cancer'   : breast cancer wisconsin dataset (classification)
-    
+
     The scikit-learn functions return a data bunch including the following items:
     - data              the features matrix
     - target            the ground truth labels
     - DESCR             a description of the dataset
     - feature_names     header for data
-    
+
     The features (and their names) are stored with the target labels in a DataFrame.
 
     For further details see https://scikit-learn.org/stable/datasets/index.html#toy-datasets
-    
+
     :param context:    function execution context
-    :param dataset:    name of the dataset to load 
+    :param dataset:    name of the dataset to load
+    :param format:     output format: pq or csv
+    :param name:       artifact name (defaults to dataset)
     :param params:     params of the sklearn load_data method
-    :param format:     save format
     """
-    os.makedirs(context.artifact_path, exist_ok=True)
-    
-    filepath = os.path.join(context.artifact_path, dataset) + '.parquet'
-    
-    # check to see if we haven't already downloaded the file
-    if not os.path.isfile(filepath):
-        artifact_path = context.artifact_path
 
-        # reach into module and import the appropriate load_xxx function
-        pkg_module = 'sklearn.datasets'
-        fname = f'load_{dataset}'
+    # reach into module and import the appropriate load_xxx function
+    pkg_module = 'sklearn.datasets'
+    fname = f'load_{dataset}'
 
-        pkg_module = __import__(pkg_module, fromlist=[fname])
-        load_data_fn = getattr(pkg_module, fname)
-        
-        data = load_data_fn(**params)
-        feature_names = data['feature_names']
+    pkg_module = __import__(pkg_module, fromlist=[fname])
+    load_data_fn = getattr(pkg_module, fname)
 
-        # save
-        xy = np.concatenate([data['data'], data['target'].reshape(-1, 1)], axis=1)
-        if hasattr(feature_names, 'append'):
-            # its a list
-            feature_names.append('labels')
-        else:
-            # its an array
-            feature_names = np.append(feature_names, 'labels')
-        df = pd.DataFrame(data=xy, columns=feature_names)
-        df.to_parquet(filepath, engine='pyarrow', index=False)
-        
-    # either we just downloaded file, or it exists, log it:
-    context.log_artifact(dataset, local_path=filepath.split('/')[-1])
-        
+    data = load_data_fn(**params)
+    feature_names = data['feature_names']
+
+    # create the toy dataset
+    xy = np.concatenate([data['data'], data['target'].reshape(-1, 1)], axis=1)
+    if hasattr(feature_names, 'append'):
+        # its a list
+        feature_names.append('labels')
+    else:
+        # its an array
+        feature_names = np.append(feature_names, 'labels')
+    df = pd.DataFrame(data=xy, columns=feature_names)
+
+    # log and upload the dataset
+    context.log_dataset(name or dataset, df=df, format=format, index=False)

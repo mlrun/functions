a33adda04ccd1d30b0975822223866242edc1859
Yasha Borstein <yashab@iguazio.com>
2020-03-26 13:54:18 +0000

    api fixes sklearn_classfier, running

--- conda-setup.sh
+++ conda-setup.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-
+# for some reason it is not 
+cd $HOME
 conda config --add channels conda-forge
 conda config --add channels anaconda
-conda create -n stable numpy pandas scipy scikit-learn matplotlib seaborn pytest kfp pyarrow
-conda install -n stable ipykernel
-conda install -n stable -c DistrictDataLabs yellowbrick # to deprecate
+conda create --yes -n mlrun-v0.4.6 numpy pandas scipy scikit-learn matplotlib seaborn pytest kfp pyarrow
+conda install --yes -n mlrun-v0.4.6 ipykernel
+conda install --yes -n mlrun-v0.4.6 -c DistrictDataLabs yellowbrick # to deprecate
 
 eval "$(conda shell.bash hook)"
-conda activate stable
-python -m ipykernel install --user --name=stable
+conda activate mlrun-v0.4.6
+python -m ipykernel install --user --name=mlrun-v0.4.6
 python -m pip install git+https://github.com/mlrun/mlrun.git@development
 python -m pip install git+https://github.com/yjb-ds/mlutils.git
-git clone https://github.com/functions/functions.git@development functions-tst
+git clone https://github.com/functions/functions.git functions-tst

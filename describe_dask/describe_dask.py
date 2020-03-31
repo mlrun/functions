86b2299cbb3b259cc5111ed526a44556da48014b
yashab <yashab@iguazio.com>
2020-03-31 15:52:37 +0000

    load_dask debuuged and running, describe_dask WIP

--- describe_dask/describe_dask.py
+++ describe_dask/describe_dask.py
@@ -25,33 +25,59 @@ from mlrun.execution import MLClientCtx
 from mlrun.datastore import DataItem
 from mlrun.artifacts import ChartArtifact, TableArtifact, PlotArtifact
 
-from typing import IO, AnyStr, Union, List, Optional
+from typing import List, Optional
 
 def table_summary(
     context: MLClientCtx,
-    dask_client: Union[DataItem, str],
-    dask_key: str = 'my_dask_dataframe',
-    target_path: str = '',
-    name: str = 'table_summary.csv',
-    key: str = 'table_summary'
+    dask_key: str = "dask_key",
+    label_column: str = "labels",
+    class_labels: List[str] = [],
+    plot_hist: bool = True,
+    plots_dest: str = "plots"
+    alt_scheduler: str = None
 ) -> None:
     """Summarize a table
     
+    Connects to dask client through the function context, or through an optional
+    user-supplied scheduler.
+
     :param context:         the function context
-    :param dask_client:     path to the dask client scheduler json file, as
-                            string or artifact
-    :param dask_key:        key of dataframe in dask client 'datasets' attribute
-    :param target_path:     destimation folder for table summary file
-    :param name:            name of table summary file (with extension like .csv)
-    :param key:             key of table summary in artifact store
+    :param dask_key:        key of dataframe in dask client "datasets" attribute
+    :param label_column:    ground truth column label
+    :param class_labels:    label for each class in tables and plots
+    :param plot_hist:       (True) set this to False for large tables
+    :param plots_dest:      destination folder of summary plots (relative to artifact_path)
+    :param alt_scheduler:   (None) an alternative scheduler file to connect with
     """
-    print(context.__dict__)
-    dask_client = Client(scheduler_file=str(dask_client))
-    df = dask_client.get_dataset('dask_key')
-    print(df.head())
-    dscr = df.describe() 
+    # no client, 
+    if not context.dask_client or alt_scheduler:
+        dask_client = Client(scheduler_file=str(alt_scheduler))
+    elif context.dask_client:
+        dask_client = Client(scheduler_file=str(context.dask_client))
+    else:
+        raise Exception("out of luck, no dask_client or scheduler file!")
+        
+    if dask_key in dask_client.datasets
+        df = dask_client.get_dataset(dask_key)
+    else:
+        context.logger.info(f"only these datasets are available {dask_client.datasets} in client {dask_client}")
+        raise Exception("dataset not found on dask cluster")
+
+    _gcf_clear(plt)   
+    labels = table.pop(label_column)
+    if not class_labels:
+        class_labels = labels.unique()
+    class_balance_model = ClassBalance(labels=class_labels)
+    class_balance_model.fit(labels)   
+    scale_pos_weight = class_balance_model.support_[0]/class_balance_model.support_[1]
+    context.log_result("scale_pos_weight", f"{scale_pos_weight:0.2f}")
+    context.log_artifact(PlotArtifact("imbalance", body=plt.gcf()), local_path=f"{plots_dest}/imbalance.html")
     
-    filepath = os.path.join(target_path, name)
-    dd.to_csv(dscr, filepath, single_file=True, index=False)
-    context.log_artifact(key, target_path=filepath)
-    
+    _gcf_clear(plt)
+    tblcorr = table.corr()
+    ax = plt.axes()
+    sns.heatmap(tblcorr, ax=ax, annot=False, cmap=plt.cm.Reds)
+    ax.set_title("features correlation")
+    context.log_artifact(PlotArtifact("correlation",  body=plt.gcf()), local_path=f"{plots_dest}/corr.html")
+    # otherwise shows last plot:
+    _gcf_clear(plt)    

c4f66f2f11d6ad04aa66ac023dc25679345242f7
Or Zilberman <Ziilbermanor@gmail.com>
2020-07-23 07:53:33 +0000

    changed describe to tries to warn

--- describe/describe.py
+++ describe/describe.py
@@ -41,37 +41,44 @@ def summarize(
     header = df.columns.values
     extra_data = {}
     
-    gcf_clear(plt)
-    snsplt = sns.pairplot(df, hue=label_column)#, diag_kws={"bw": 1.5})
-    extra_data["histograms"] = context.log_artifact(PlotArtifact("histograms",  body=plt.gcf()),
-                                                    local_path=f"{plots_dest}/hist.html", db_key=False)
+    try:
+        gcf_clear(plt)
+        snsplt = sns.pairplot(df, hue=label_column)#, diag_kws={"bw": 1.5})
+        extra_data["histograms"] = context.log_artifact(PlotArtifact("histograms",  body=plt.gcf()),
+                                                        local_path=f"{plots_dest}/hist.html", db_key=False)
+    except Exception as e:
+        context.logger.error(f'Failed to create pairplot histograms due to: {e}')
     
+    try:
+        gcf_clear(plt)
+        plot_cols = 3
+        plot_rows = int((len(header) - 1) / plot_cols)+1
+        fig, ax = plt.subplots(plot_rows, plot_cols, figsize=(15, 4))
+        fig.tight_layout(pad=2.0)
+        for i in range(plot_rows * plot_cols):
+            if i < len(header):
+                sns.violinplot(x=df[header[i]], ax=ax[int(i / plot_cols)][i % plot_cols], 
+                            orient='h', width=0.7, inner="quartile")
+            else:
+                fig.delaxes(ax[int(i / plot_cols)][i % plot_cols])        
+            i+=1
+        extra_data["violin"] = context.log_artifact(PlotArtifact("violin",  body=plt.gcf(), title='Violin Plot'),
+                                                    local_path=f"{plots_dest}/violin.html", db_key=False)
+    except Exception as e:
+        context.logger.warn(f'Failed to create violin distribution plots due to: {e}')
 
-    gcf_clear(plt)
-    plot_cols = 3
-    plot_rows = int((len(header) - 1) / plot_cols)+1
-    fig, ax = plt.subplots(plot_rows, plot_cols, figsize=(15, 4))
-    fig.tight_layout(pad=2.0)
-    for i in range(plot_rows * plot_cols):
-        if i < len(header):
-            sns.violinplot(x=df[header[i]], ax=ax[int(i / plot_cols)][i % plot_cols], 
-                           orient='h', width=0.7, inner="quartile")
-        else:
-            fig.delaxes(ax[int(i / plot_cols)][i % plot_cols])        
-        i+=1
-    extra_data["violin"] = context.log_artifact(PlotArtifact("violin",  body=plt.gcf(), title='Violin Plot'),
-                                                local_path=f"{plots_dest}/violin.html", db_key=False)
-    
-    
-    if label_column:
-        gcf_clear(plt)   
+    if label_column: 
         labels = df.pop(label_column)
         imbtable = labels.value_counts(normalize=True).sort_index()
-        balancebar = imbtable.plot(kind='bar', title='class imbalance - labels')
-        balancebar.set_xlabel('class')
-        balancebar.set_ylabel("proportion of total")
-        extra_data["imbalance"] = context.log_artifact(PlotArtifact("imbalance", body=plt.gcf()), 
-                                                       local_path=f"{plots_dest}/imbalance.html")
+        try:
+            gcf_clear(plt)  
+            balancebar = imbtable.plot(kind='bar', title='class imbalance - labels')
+            balancebar.set_xlabel('class')
+            balancebar.set_ylabel("proportion of total")
+            extra_data["imbalance"] = context.log_artifact(PlotArtifact("imbalance", body=plt.gcf()), 
+                                                        local_path=f"{plots_dest}/imbalance.html")
+        except Exception as e:
+            context.logger.warn(f'Failed to create class imbalance plot due to: {e}')
         context.log_artifact(TableArtifact("imbalance-weights-vec", 
                                            df=pd.DataFrame({"weights": imbtable})),
                              local_path=f"{plots_dest}/imbalance-weights-vec.csv", db_key=False)
@@ -85,12 +92,15 @@ def summarize(
     context.log_artifact(TableArtifact("correlation-matrix", df=tblcorr, visible=True), 
                          local_path=f"{plots_dest}/correlation-matrix.csv", db_key=False)
     
-    gcf_clear(plt)
-    ax = plt.axes()
-    sns.heatmap(tblcorr, ax=ax, mask=mask, annot=False, cmap=plt.cm.Reds)
-    ax.set_title("features correlation")
-    extra_data["correlation"] = context.log_artifact(PlotArtifact("correlation",  body=plt.gcf(), title='Correlation Matrix'),
-                                                     local_path=f"{plots_dest}/corr.html", db_key=False)
+    try:
+        gcf_clear(plt)
+        ax = plt.axes()
+        sns.heatmap(tblcorr, ax=ax, mask=mask, annot=False, cmap=plt.cm.Reds)
+        ax.set_title("features correlation")
+        extra_data["correlation"] = context.log_artifact(PlotArtifact("correlation",  body=plt.gcf(), title='Correlation Matrix'),
+                                                        local_path=f"{plots_dest}/corr.html", db_key=False)
+    except Exception as e:
+            context.logger.warn(f'Failed to create features correlation plot due to: {e}')
     
 
     gcf_clear(plt)

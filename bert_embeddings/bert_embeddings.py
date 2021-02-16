bf92975a5f53094c9deaa64c756b4c9fcdc7b57a
alon <maoralo@post.bgu.ac.il>
2021-02-16 08:32:39 +0000

    blacked fmt

--- bert_embeddings/bert_embeddings.py
+++ bert_embeddings/bert_embeddings.py
@@ -4,21 +4,24 @@ from typing import Union, List
 import json
 import pickle
 
+
 def init_context(context):
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model = BertModel.from_pretrained('bert-base-uncased')
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    model = BertModel.from_pretrained("bert-base-uncased")
     model.eval()
-    
-    setattr(context.user_data, 'tokenizer', tokenizer)
-    setattr(context.user_data, 'model', model)
+
+    setattr(context.user_data, "tokenizer", tokenizer)
+    setattr(context.user_data, "model", model)
+
 
 def handler(context, event):
     docs = json.loads(event.body)
     docs = [doc.lower() for doc in docs]
-    docs = context.user_data.tokenizer.batch_encode_plus(docs, pad_to_max_length=True, return_tensors='pt')
-    
+    docs = context.user_data.tokenizer.batch_encode_plus(
+        docs, pad_to_max_length=True, return_tensors="pt"
+    )
+
     with torch.no_grad():
         embeddings = context.user_data.model(**docs)
     embeddings = [embeddings[0].numpy(), embeddings[1].numpy()]
     return pickle.dumps(embeddings)
-

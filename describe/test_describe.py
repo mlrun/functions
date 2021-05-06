41d06c06a8fe36bafd69cf87c70d5b78213fd30c
Avi Asulin <avia@iguazio.com>
2021-05-06 09:35:36 +0300

    update describe

--- describe/test_describe.py
+++ describe/test_describe.py
@@ -1,5 +1,10 @@
 from describe import summarize
+from mlrun import MLClientCtx
+
+DATA_URL = 'https://s3.wasabisys.com/iguazio/data/iris/iris_dataset.csv'
 
 
 def test_summer():
-    raise NotImplemented
+    ctx = MLClientCtx()
+    summarize(MLClientCtx, table=DATA_URL, label_column='label',update_dataset=True)
+

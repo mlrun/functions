a9c3892c333e2e17cb896cd1457a7ad79b532268
Michael Livshits <livsmichael@gmail.com>
2021-05-06 12:26:50 +0300

    Added support for legacy catalog creation, experimental move to Pipfile instead of requirements.txt

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import click
 
+from common.create_legacy_catalog import create_legacy_catalog
 from common.item_to_function import item_to_function
 from common.marketplace.build_docs import build_docs
 from common.new_item import new_item
@@ -15,6 +16,7 @@ cli.add_command(new_item)
 cli.add_command(item_to_function)
 cli.add_command(test_suite)
 cli.add_command(build_docs)
+cli.add_command(create_legacy_catalog)
 
 if __name__ == "__main__":
     cli()

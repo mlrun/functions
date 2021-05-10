12af730e3bc189ec99894d858cb02fa6714c0ca7
Avi Asulin <avia@iguazio.com>
2021-05-10 15:38:15 +0300

    add clean to test artifacts

--- arc_to_parquet/test_arc_to_parquet.py
+++ arc_to_parquet/test_arc_to_parquet.py
@@ -1,8 +1,18 @@
 from mlrun import code_to_function
+from pathlib import Path
+import shutil
 import os
 
 ARCHIVE = "https://archive.ics.uci.edu/ml/machine-learning-databases/00280/HIGGS.csv.gz"
 ARTIFACTS_PATH = 'artifacts'
+RUNS_PATH = 'runs'
+SCHEDULES_PATH = 'schedules'
+
+
+def _delete_outputs(paths):
+    for path in paths:
+        if Path(path).is_dir():
+            shutil.rmtree(path)
 
 
 def test_arc_to_parquet():
@@ -18,3 +28,6 @@ def test_arc_to_parquet():
            local=True
 
            )
+    _delete_outputs({ARTIFACTS_PATH, RUNS_PATH, SCHEDULES_PATH})
+
+

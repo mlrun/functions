16e346ea0e3dd402846d133c77ba63ab2ea93e93
Michael Livshits <livsmichael@gmail.com>
2021-05-18 15:30:04 +0300

    Updated marketplace building utility to support multiple source, moved _static directory to channel level, updated CI with coresponding changes

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import click
 
 from cli.create_legacy_catalog import create_legacy_catalog
 from cli.item_to_function import item_to_function
-from cli.marketplace.build_docs import build_docs
+from cli.marketplace.build import build_marketplace_cli
 from cli.new_item import new_item
 from cli.test_suite import test_suite
 
@@ -13,9 +13,9 @@ def cli():
 
 
 cli.add_command(new_item)
-cli.add_command(item_to_function)
-cli.add_command(test_suite)
-cli.add_command(build_docs)
+cli.add_command(item_to_function, name="update-function-yaml")
+cli.add_command(test_suite, name="run-tests")
+cli.add_command(build_marketplace_cli, name="build-marketplace")
 cli.add_command(create_legacy_catalog)
 
 if __name__ == "__main__":

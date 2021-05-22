bac01aa6aec18fbf685b4583dde8679d2465767c
Michael Livshits <livsmichael@gmail.com>
2021-05-23 02:36:52 +0300

    Updated churn_server content

--- churn_server/test_churn_server.py
+++ churn_server/test_churn_server.py
@@ -7,40 +7,46 @@ import mlrun
 from pygit2 import Repository
 
 
-MODEL_PATH = os.path.join(os.path.abspath('./'), 'models')
+MODEL_PATH = os.path.join(os.path.abspath("./"), "models")
 MODEL = MODEL_PATH + "model.pt"
 
 
 def set_mlrun_hub_url():
-    branch = Repository('.').head.shorthand
+    branch = Repository(".").head.shorthand
     hub_url = "https://raw.githubusercontent.com/mlrun/functions/{}/churn_server/function.yaml".format(
-        branch)
+        branch
+    )
     mlrun.mlconf.hub_url = hub_url
 
 
 def download_pretrained_model(model_path):
     # Run this to download the pre-trained model to your `models` directory
     import os
+
     model_location = None
     saved_models_directory = model_path
     # Create paths
     os.makedirs(saved_models_directory, exist_ok=1)
-    model_filepath = os.path.join(saved_models_directory, os.path.basename(model_location))
+    model_filepath = os.path.join(
+        saved_models_directory, os.path.basename(model_location)
+    )
     wget.download(model_location, model_filepath)
 
 
 def test_local_churn_server():
     set_mlrun_hub_url()
-    model_path = os.path.join(os.path.abspath('./'), 'models')
-    model = model_path+'/model.pt'
+    model_path = os.path.join(os.path.abspath("./"), "models")
+    model = model_path + "/model.pt"
     if not path.exists(model):
         raise NotImplemented
-    fn = import_function('hub://churn_server')
-    fn.add_model('mymodel', model_path=model, class_name='ChurnModel')
+    fn = import_function("hub://churn_server")
+    fn.add_model("mymodel", model_path=model, class_name="ChurnModel")
     # create an emulator (mock server) from the function configuration)
     server = fn.to_mock_server()
 
-    instances = ['I had a pleasure to work with such dedicated team. Looking forward to \
-                 cooperate with each and every one of them again.']
+    instances = [
+        "I had a pleasure to work with such dedicated team. Looking forward to \
+                 cooperate with each and every one of them again."
+    ]
     result = server.test("/v2/models/mymodel/infer", {"instances": instances})
     assert result[0] == 2

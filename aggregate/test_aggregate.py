a216ee150f455221f456c99e68f4f9c9603aa29b
daniels <daniels@iguazio.com>
2021-06-06 17:49:41 +0300

    changed tests to proper align with requirments, changed categorie to ETL

--- aggregate/test_aggregate.py
+++ aggregate/test_aggregate.py
@@ -1,36 +1,19 @@
 from pathlib import Path
-import shutil
 from mlrun import code_to_function
 import mlrun
-import re
-import subprocess
-from pygit2 import Repository
-
+from functions.cli.helpers import set_mlrun_hub_url, delete_outputs
+import pandas as pd
 
 METRICS_PATH = 'data/metrics.pq'
 ARTIFACTS_PATH = 'artifacts'
 RUNS_PATH = 'runs'
 SCHEDULES_PATH = 'schedules'
+FUNCTION_PATH = 'functions'
 AGGREGATE_PATH = 'artifacts/aggregate.pq'
 
-def _delete_outputs(paths):
-    for path in paths:
-        if Path(path).is_dir():
-            shutil.rmtree(path)
 
-def _set_mlrun_hub_url(repo_name = None, branch_name = None, function_name = None):
-    repo_name =  re.search("\.com/.*?/", str(subprocess.run(['git', 'remote', '-v'], stdout=subprocess.PIPE).stdout)).group()[5:-1] if not repo_name else repo_name
-    branch_name = Repository('.').head.shorthand if not branch_name else branch_name
-    function_name = "" if not function_name else function_name # MUST ENTER FUNCTION NAME !!!!
-    hub_url = f"https://raw.githubusercontent.com/{repo_name}/functions/{branch_name}/{function_name}/function.yaml"
-    mlrun.mlconf.hub_url = hub_url
-
-def test_run_local_aggregate():
-    fn = code_to_function(name='test_aggregate',
-                          filename="aggregate.py",
-                          handler="aggregate",
-                          kind="job",
-                          )
+def test_local_aggregate():
+    fn = mlrun.import_function("function.yaml")
     fn.run(params={'metrics': ['cpu_utilization'],
                    'labels': ['is_error'],
                    'metric_aggs': ['mean', 'sum'],
@@ -44,11 +27,17 @@ def test_run_local_aggregate():
            , local=True
            , inputs={'df_artifact': METRICS_PATH}
            )
-    assert Path(AGGREGATE_PATH).is_file()
-    _delete_outputs({ARTIFACTS_PATH, RUNS_PATH, SCHEDULES_PATH})
+    df = pd.read_parquet("data/metrics.pq")
+    aggregated_df = pd.read_parquet("artifacts/aggregate.pq")
+    assert(aggregated_df["cpu_utilization_mean_daily"].tolist() == df["cpu_utilization"].rolling(5).mean().dropna().tolist()) is True
+    assert(aggregated_df["cpu_utilization_sum_daily"].tolist() == df["cpu_utilization"].rolling(5).sum().dropna().tolist()) is True
+    # assert(aggregated_df["is_error_max_daily"].tolist() == df["cpu_utilization"].rolling(5).max().dropna().tolist()) is True
+    # last test is very slow !!
+    delete_outputs({ARTIFACTS_PATH, RUNS_PATH, SCHEDULES_PATH,FUNCTION_PATH})
 
-def test_run_imported_aggregate():
-    _set_mlrun_hub_url(function_name="aggregate")
+
+def test_hub_imported_aggregate():
+    set_mlrun_hub_url(function_name="aggregate")
     fn = mlrun.import_function("hub://aggregate")
     fn.run(params={'metrics': ['cpu_utilization'],
                    'labels': ['is_error'],
@@ -63,6 +52,11 @@ def test_run_imported_aggregate():
            , local=True
            , inputs={'df_artifact': METRICS_PATH}
            )
-    assert Path(AGGREGATE_PATH).is_file()
-    _delete_outputs({ARTIFACTS_PATH, RUNS_PATH, SCHEDULES_PATH})
+    df = pd.read_parquet("data/metrics.pq")
+    aggregated_df = pd.read_parquet("artifacts/aggregate.pq")
+    assert(aggregated_df["cpu_utilization_mean_daily"].tolist() == df["cpu_utilization"].rolling(5).mean().dropna().tolist()) is True
+    assert(aggregated_df["cpu_utilization_sum_daily"].tolist() == df["cpu_utilization"].rolling(5).sum().dropna().tolist()) is True
+    # assert(aggregated_df["is_error_max_daily"].tolist() == df["cpu_utilization"].rolling(5).max().dropna().tolist()) is True
+    # last test is very slow !!
+    delete_outputs({ARTIFACTS_PATH, RUNS_PATH, SCHEDULES_PATH,FUNCTION_PATH})
 

fbc36a27c277051dfab67ab3cae5f77a07360418
daniels <daniels@iguazio.com>
2021-06-09 16:39:17 +0300

    added local+hub tests, deleting remaning files, fixed req+item.yaml

--- xgb_custom/test_xgb_custom.py
+++ xgb_custom/test_xgb_custom.py
@@ -1,38 +1,53 @@
-from mlrun import code_to_function
+from mlrun import import_function
+import os
+from functions.cli.helpers import delete_outputs,set_mlrun_hub_url
 
 
-def test_xgb_custom():
-    fn = code_to_function(
-        name="test_xgb_custom",
-        filename="xgb_custom.py",
-        handler="gen_outliers",
-        kind="job",
-    )
+ARTIFACT_PATH = "artifacts"
+FUNCTION_PATH = "functions"
+PLOTS_PATH = "plots"
+RUNS_PATH = "runs"
+SCHEDULES_PATH = "schedules"
 
-    outliers_run = fn.run(
-        params={
-            "nrows": 8192,
-            "label_type": "float",
-            "local_path": "./artifacts/inputs/xgb_custom",
-        },
-        local=True,
-    )
 
-    fn = code_to_function(
-        name="test_fit_model",
-        filename="xgb_custom.py",
-        handler="fit",
-        kind="job",
-    )
+def test_local_xgb_custom():
+    fn = import_function("function.yaml")
+    fn.run(params={"nrows": 8192,
+                   "label_type": "float",
+                   "local_path": "./artifacts/inputs/xgb_custom"},
+           handler="gen_outliers",
+           local=True)
 
-    fit_run = fn.run(
-        params={
-            "num_boost_round": 40,
-            "verbose_eval": False,
-            "XGB_max_depth": 2,
-            "XGB_subsample": 0.9,
-            "test_set_key": "./artifacts/inputs/test-set",
-        },
-        inputs={"dataset": "./artifacts/inputs/xgb_custom.parquet"},
-        local=True,
-    )
+    fn.run(params={"num_boost_round": 40,
+                   "verbose_eval": False,
+                   "XGB_max_depth": 2,
+                   "XGB_subsample": 0.9,
+                   "test_set_key": "./artifacts/inputs/test-set"},
+           inputs={"dataset": "./artifacts/inputs/xgb_custom.parquet"},
+           handler="fit",
+           local=True)
+
+    assert(os.path.exists(os.getcwd() + "/plots/learning-curves.html"))
+    delete_outputs({ARTIFACT_PATH, FUNCTION_PATH, PLOTS_PATH, RUNS_PATH, SCHEDULES_PATH})
+
+
+def test_hub_xgb_custom():
+    set_mlrun_hub_url(function_name="xgb_custom")
+    fn = import_function("hub://xgb_custom")
+    fn.run(params={"nrows": 8192,
+                   "label_type": "float",
+                   "local_path": "./artifacts/inputs/xgb_custom"},
+           handler="gen_outliers",
+           local=True)
+
+    fn.run(params={"num_boost_round": 40,
+                   "verbose_eval": False,
+                   "XGB_max_depth": 2,
+                   "XGB_subsample": 0.9,
+                   "test_set_key": "./artifacts/inputs/test-set"},
+           inputs={"dataset": "./artifacts/inputs/xgb_custom.parquet"},
+           handler="fit",
+           local=True)
+
+    assert (os.path.exists(os.getcwd() + "/plots/learning-curves.html"))
+    delete_outputs({ARTIFACT_PATH, FUNCTION_PATH, PLOTS_PATH, RUNS_PATH, SCHEDULES_PATH})

d1fca721876fba83d34d1233b43f5e992b8ad519
daniels <daniels@iguazio.com>
2021-06-09 11:06:59 +0300

    added assert constraints

--- bert_embeddings/test_bert_embeddings.py
+++ bert_embeddings/test_bert_embeddings.py
@@ -15,5 +15,6 @@ def test_local_bert_embeddings():
     ctx = nuclio.Context()
     init_context(ctx)
     outputs = pickle.loads(handler(ctx, event))
-    assert(abs(np.mean(outputs[0]) - -0.011996539) <= 0.0001)
+    assert(True if abs(np.mean(outputs[0]) - -0.011996539) <= 0.0001 else False) is True
+    assert (True if abs(np.mean(outputs[0]) - -0.011996539) > 0 else False) is True
     delete_outputs({FUNCTION_PATH, SCHEDULE_PATH, RUNS_PATH})

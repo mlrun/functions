375eddd213c4ffe19753e0b036564cac97989428
Avi Asulin <avia@iguazio.com>
2021-06-15 14:36:53 +0300

    update xgb trainer with proper unit-test

--- xgb_trainer/test_xgb_trainer.py
+++ xgb_trainer/test_xgb_trainer.py
@@ -1,4 +1,4 @@
-from mlrun import code_to_function
+from mlrun import code_to_function, import_function
 import os
 
 
@@ -17,7 +17,7 @@ def get_class_data():
         "file_ext": "csv"}, local=True, artifact_path="./artifacts/inputs")
 
 
-def test_xgb_trainer():
+def test_xgb_trainer_code_to_function():
     get_class_data()
     fn = code_to_function(name='test_xgb_trainer',
                           filename="xgb_trainer.py",
@@ -36,3 +36,27 @@ def test_xgb_trainer():
 
     assert(os.path.exists(os.getcwd() + "/models/model.pkl"))
 
+
+def test_local_xgb_trainer_import_function():
+    # importing data preparation function locally
+    fn = import_function("../gen_class_data/function.yaml")
+    fn.run(params={
+        "n_samples": 10_000,
+        "m_features": 5,
+        "k_classes": 2,
+        "weight": [0.5, 0.5],
+        "sk_params": {"n_informative": 2},
+        "file_ext": "csv"}, local=True, artifact_path="./artifacts/inputs")
+
+    fn = import_function("function.yaml")
+    fn.run(params={
+        "model_type": "classifier",
+        "CLASS_tree_method": "hist",
+        "CLASS_objective": "binary:logistic",
+        "CLASS_booster": "gbtree",
+        "FIT_verbose": 0,
+        "label_column": "labels",
+        "test_set": "./artifacts/test-set"},
+        local=True, inputs={"dataset": './artifacts/inputs/classifier-data.csv'})
+
+    assert (os.path.exists(os.getcwd() + "/models/model.pkl"))

4ebc9d325cf28893276704deac6b781d4b1162a2
Avi Asulin <avia@iguazio.com>
2021-07-07 17:54:12 +0300

    updating describe test

--- describe_dask/test_describe_dask.py
+++ describe_dask/test_describe_dask.py
@@ -21,41 +21,34 @@ def _validate_paths(paths: {}):
             raise FileNotFoundError
 
 
-# def test_describe_dask_local():
-#     project_name = "dask-demo"
-#     project_path = path.abspath(project_name)
-#     mlrun.set_environment(project=project_name, artifact_path='./', api_path=project_path)
-#     dask_cluster_name = "dask-cluster"
-#     dask_cluster = new_function(dask_cluster_name, kind='dask', image='mlrun/ml-models')
-#     # function URI is db://<project>/<name>
-#     #dask_uri = f'db://{project_name}/{dask_cluster_name}'
-#     dask_uri = "dask-demo/functions/dask-demo/test-describe-dask/latest.yaml"
-#     #client = mlrun.import_function(dask_ri).client
-#     if Path(PLOTS_PATH).is_dir():
-#         shutil.rmtree(PLOTS_PATH)
-#     fn = code_to_function(name='test_describe_dask',
-#                           filename="describe_dask.py",
-#                           handler="summarize",
-#                           kind="local",
-#                           )
-#     fn.spec.command = "describe_dask.py"
-#     fn.run(inputs={"table": DATA_URL},
-#                     params={'update_dataset': True,
-#                             'label_column': 'label'
-#                             ,'dask_function': dask_uri
-#
-#                             }
-#                    ,handler = "summarize"
-#
-#             ,artifact_path='artifacts'
-#            )
-#     # task = new_task(name="task-describe",
-#     #                 handler=summarize,
-#     #                 )
-#     # run_local(task)
-#     _validate_paths({'corr.html',
-#                      'correlation-matrix.csv',
-#                      'hist.html',
-#                      'imbalance.html',
-#                      'imbalance-weights-vec.csv',
-#                      'violin.html'})
+def test_describe_dask_local():
+    dask_cluster_name = "dask-cluster"
+    dask_cluster = new_function(dask_cluster_name, kind='dask', image='mlrun/ml-models')
+    dask_uri = "dask_func.yaml"
+    dask_cluster.export(dask_uri)
+    fn = code_to_function(name='test_describe_dask',
+                          filename="describe_dask.py",
+                          handler="summarize",
+                          kind="local",
+                          )
+    fn.spec.command = "describe_dask.py"
+    fn.run(inputs={"table": DATA_URL},
+                    params={'update_dataset': True,
+                            'label_column': 'label'
+                            ,'dask_function': dask_uri
+
+                            }
+                   ,handler = "summarize"
+
+            ,artifact_path='artifacts'
+           )
+    # task = new_task(name="task-describe",
+    #                 handler=summarize,
+    #                 )
+    # run_local(task)
+    _validate_paths({'corr.html',
+                     'correlation-matrix.csv',
+                     'hist.html',
+                     'imbalance.html',
+                     'imbalance-weights-vec.csv',
+                     'violin.html'})

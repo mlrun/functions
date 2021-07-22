513d2c376193cc425a392046d94120b875502887
Michael Livshits <livsmichael@gmail.com>
2021-07-22 14:49:41 +0300

    changed storage options

--- model_monitoring_stream/model_monitoring_stream.py
+++ model_monitoring_stream/model_monitoring_stream.py
@@ -278,7 +278,7 @@ class EventStreamProcessor:
                         # Settings for v3io storage
                         storage_options={
                             "v3io_api": self.v3io_api,
-                            "v3io_access_key": self.model_monitoring_access_key,
+                            "access_key": self.model_monitoring_access_key,
                         },
                     ),
                 ],

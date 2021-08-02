ce149ced2d2b5574d9171cc22fe002f6218d92f7
Avi Asulin <avia@iguazio.com>
2021-08-02 17:28:23 +0300

    add regression model example

--- xgb_trainer/test_xgb_trainer.py
+++ xgb_trainer/test_xgb_trainer.py
@@ -1,4 +1,4 @@
-from mlrun import code_to_function, import_function
+from mlrun import code_to_function, import_function, DataItem
 import os
 
 
@@ -22,8 +22,9 @@ def test_xgb_trainer_code_to_function():
     fn = code_to_function(name='test_xgb_trainer',
                           filename="xgb_trainer.py",
                           handler="train_model",
-                          kind="job",
+                          kind="local",
                           )
+    fn.spec.command="xgb_trainer.py"
     fn.run(params= {
         "model_type": "classifier",
         "CLASS_tree_method": "hist",
@@ -32,7 +33,8 @@ def test_xgb_trainer_code_to_function():
         "FIT_verbose": 0,
         "label_column": "labels",
         "test_set": "./artifacts/test-set"},
-        local=True, inputs={"dataset": './artifacts/inputs/classifier-data.csv'})
+        #local=True,
+        inputs={"dataset": './artifacts/inputs/classifier-data.csv'})
 
     assert(os.path.exists(os.getcwd() + "/models/model.pkl"))
 
@@ -60,3 +62,35 @@ def test_local_xgb_trainer_import_function():
         local=True, inputs={"dataset": './artifacts/inputs/classifier-data.csv'})
 
     assert (os.path.exists(os.getcwd() + "/models/model.pkl"))
+
+
+def test_xgb_trainer_regression_code_to_function():
+    from sklearn.datasets import load_boston
+    boston = load_boston()
+    import pandas as pd
+    data = pd.DataFrame(boston.data)
+    data.columns = boston.feature_names
+    data['PRICE'] = boston.target
+    data.to_csv("boston.csv")
+
+
+    fn = code_to_function(name='test_xgb_trainer',
+                          filename="xgb_trainer.py",
+                          handler="train_model",
+                          kind="local",
+                          )
+    fn.spec.command="xgb_trainer.py"
+    fn.run(params= {
+        "model_type": "regressor",
+                        #"CLASS_tree_method": "hist",
+                        "CLASS_objective": "reg:linear",
+                        #"CLASS_booster": "gbtree",
+                        "FIT_verbose": 0,
+                        "label_column": "PRICE",
+
+        "test_set": "./artifacts/test-set"},
+        inputs={"dataset": 'boston.csv'}
+        #local=True,
+    )
+
+    assert(os.path.exists(os.getcwd() + "/models/model.pkl"))

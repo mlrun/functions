b876f152c0b5ad367506a12ed6dfda866ca4e633
Michael Livshits <livsmichael@gmail.com>
2021-08-16 11:22:59 +0300

    Fixed batch function

--- model_monitoring_batch/model_monitoring_batch.py
+++ model_monitoring_batch/model_monitoring_batch.py
@@ -1,10 +1,12 @@
 import json
+import os
 from collections import defaultdict
 from dataclasses import dataclass
 from typing import Optional, List, Dict
 
 import numpy as np
 import pandas as pd
+import v3io
 from mlrun import get_run_db
 from mlrun import store_manager
 from mlrun.data_types.infer import DFDataInfer, InferOptions
@@ -212,14 +214,24 @@ class VirtualDrift:
 
 
 class BatchProcessor:
-    def __init__(self, context: MLClientCtx, project: str):
+    def __init__(
+        self,
+        context: MLClientCtx,
+        project: str,
+        model_monitoring_access_key: str,
+        v3io_access_key: str,
+    ):
         self.context = context
         self.project = project
+
+        self.v3io_access_key = v3io_access_key
+        self.model_monitoring_access_key = (
+            model_monitoring_access_key or v3io_access_key
+        )
+
         self.virtual_drift = VirtualDrift(inf_capping=10)
 
         template = config.model_endpoint_monitoring.store_prefixes.default
-
-        self.parquet_path = template.format(project=self.project, kind="parquet")
 
         kv_path = template.format(project=self.project, kind="endpoints")
         _, self.kv_container, self.kv_path = parse_model_endpoint_store_prefix(kv_path)
@@ -234,8 +246,15 @@ class BatchProcessor:
             stream_path
         )
 
+        self.parquet_path = config.model_endpoint_monitoring.store_prefixes.user_space.format(
+            project=project, kind="parquet"
+        )
+
         logger.info(
             "Initializing BatchProcessor",
+            project=project,
+            model_monitoring_access_key_initalized=bool(model_monitoring_access_key),
+            v3io_access_key_initialized=bool(v3io_access_key),
             parquet_path=self.parquet_path,
             kv_container=self.kv_container,
             kv_path=self.kv_path,
@@ -253,28 +272,29 @@ class BatchProcessor:
         )
 
         self.db = get_run_db()
-        self.v3io = get_v3io_client()
+        self.v3io = get_v3io_client(access_key=self.v3io_access_key)
         self.frames = get_frames_client(
-            address=config.v3io_framesd, container=self.tsdb_container
+            address=config.v3io_framesd,
+            container=self.tsdb_container,
+            token=self.v3io_access_key,
         )
 
     def post_init(self):
-        try:
-            self.v3io.stream.create(
-                container=self.stream_container,
-                stream_path=self.stream_path,
-                shard_count=1,
-            )
-        except Exception as e:
-            if "ResourceInUseException" in str(e):
-                logger.error("Stream already exsits...")
-            else:
-                raise e
+        response = self.v3io.create_stream(
+            container=self.stream_container,
+            path=self.stream_path,
+            shard_count=1,
+            raise_for_status=v3io.dataplane.RaiseForStatus.never,
+            access_key=self.v3io_access_key,
+        )
+
+        if not (response.status_code == 400 and "ResourceInUse" in str(response.body)):
+            response.raise_for_status([409, 204, 403])
 
     def run(self):
 
         try:
-            endpoints = self.db.list_endpoints(self.project)
+            endpoints = self.db.list_model_endpoints(self.project)
         except Exception as e:
             logger.error("Failed to list endpoints", exc=e)
             return
@@ -306,7 +326,7 @@ class BatchProcessor:
 
                 logger.info(f"Now processing {full_path}")
 
-                endpoint = self.db.get_endpoint(
+                endpoint = self.db.get_model_endpoint(
                     project=self.project, endpoint_id=endpoint_id
                 )
 
@@ -342,7 +362,18 @@ class BatchProcessor:
                     self.v3io.stream.put_records(
                         container=self.stream_container,
                         stream_path=self.stream_path,
-                        records=[{"drift_status": drift_status, **drift_result}],
+                        records=[
+                            {
+                                "data": json.dumps(
+                                    {
+                                        "endpoint_id": endpoint_id,
+                                        "drift_status": drift_status,
+                                        "drift_measure": drift_measure,
+                                        "drift_per_feature": {**drift_result},
+                                    }
+                                )
+                            }
+                        ],
                     )
 
                 self.v3io.kv.update(
@@ -409,7 +440,12 @@ class BatchProcessor:
         return last_dir
 
 
-def handler(context: MLClientCtx, project: str):
-    batch_processor = BatchProcessor(context, project)
+def handler(context: MLClientCtx):
+    batch_processor = BatchProcessor(
+        context=context,
+        project=context.project,
+        model_monitoring_access_key=os.environ.get("MODEL_MONITORING_ACCESS_KEY"),
+        v3io_access_key=os.environ.get("V3IO_ACCESS_KEY"),
+    )
     batch_processor.post_init()
     batch_processor.run()

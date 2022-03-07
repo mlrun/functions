3821b08dc12493132fd46b7f772f8a40734e7522
davesh0812 <davesh0812@gmail.com>
2022-03-07 18:37:15 +0200

    changed after pr review

--- describe/describe.py
+++ describe/describe.py
@@ -2,84 +2,188 @@
 
 import warnings
 
+import mlrun.artifacts
+
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
-import numpy as np
 import pandas as pd
-
-from mlrun.execution import MLClientCtx
-from mlrun.datastore import DataItem
-from mlrun.artifacts import TableArtifact, PlotlyArtifact
-import plotly.figure_factory as ff
 import plotly.express as px
+import plotly.figure_factory as ff
 import plotly.graph_objects as go
-
-from typing import List
+from mlrun.artifacts import PlotlyArtifact, TableArtifact
+from mlrun.datastore import DataItem
+from mlrun.execution import MLClientCtx
+from plotly.subplots import make_subplots
 
 pd.set_option("display.float_format", lambda x: "%.2f" % x)
+MAX_SIZE_OF_DF = 5000
 
 
-def summarize(
-        context: MLClientCtx,
-        table: DataItem,
-        label_column: str = None,
-        class_labels: List[str] = [],
-        plot_hist: bool = True,
-        plots_dest: str = "plots",
-        update_dataset=False,
-        frac: float = 0.10
+def analysis(
+    context: MLClientCtx,
+    name: str = "dataset",
+    table: DataItem = None,
+    label_column: str = None,
+    plots_dest: str = "plots",
+    update_dataset: bool = False,
+    frac: float = 0.10,
 ) -> None:
-    """Summarize a table
+    """
+    The function will output the following artifacts per
+    column within the data frame (based on data types):
 
-    :param context:         the function context
-    :param table:           MLRun input pointing to pandas dataframe (csv/parquet file path)
-    :param label_column:    ground truth column label
-    :param class_labels:    label for each class in tables and plots
-    :param plot_hist:       (True) set this to False for large tables
-    :param plots_dest:      destination folder of summary plots (relative to artifact_path)
-    :param update_dataset:  when the table is a registered dataset update the charts in-place
-    :param frac:  when the table has more than 5000 samples, the function will execute on random frac from the data
+    histogram matrix chart
+    histogram per feature chart
+    violin chart
+    correlation-matrix chart
+    correlation-matrix csv
+    imbalance pie chart
+    imbalance-weights-vec csv
+
+    :param context:                 the function context
+    :param name:                    key of dataset to database ("dataset" for default)
+    :param table:                   MLRun input pointing to pandas dataframe (csv/parquet file path)
+    :param label_column:            ground truth column label
+    :param plots_dest:              destination folder of summary plots (relative to artifact_path)
+                                    ("plots" for default)
+    :param update_dataset:          when the table is a registered dataset update the charts in-place
+    :param frac:                    when the table has more than 5000 samples,
+                                    the function will execute on random frac from the data (0.1 default)
     """
 
     df = table.as_df()
-    if df.shape[0] > 5000:
+    if df.shape[0] > MAX_SIZE_OF_DF:
         df = df.sample(frac=frac)
     extra_data = {}
 
-    # histograms
+    _create_histogram_mat_artifact(
+        context, df, extra_data, label_column, plots_dest
+    )
+    _create_features_histogram_artifacts(
+        context, df, extra_data, label_column, plots_dest
+    )
+    _create_violin_artifact(
+        context, df, extra_data, plots_dest
+                            )
+    _create_imbalance_artifact(
+        context, df, extra_data, label_column, plots_dest
+                               )
+    _create_corr_artifact(
+        context, df, extra_data, label_column, plots_dest
+                          )
+
+    artifact = table._artifact_url
+    if artifact is None:  # dataset not stored
+        artifact = context.log_dataset(key="dataset", db_key=name, stats=True, df=df)
+
+    if update_dataset:
+        from mlrun.artifacts import update_dataset_meta
+
+        update_dataset_meta(artifact, extra_data=extra_data)
+
+    # TODO : Plots according to client wishes - like preform histogram on selected features.
+    # TODO : 3-D plot on on selected features.
+    # TODO : Reintegration plot on on selected features.
+
+
+def _create_histogram_mat_artifact(
+    context: MLClientCtx,
+    df: pd.DataFrame,
+    extra_data: dict,
+    label_column: str,
+    plots_dest: str,
+):
+    """
+    Create and log a histogram matrix artifact
+    """
     try:
-        df[label_column] = df[label_column].apply(str)
-        fig = ff.create_scatterplotmatrix(df, diag='histogram', index=label_column, width=2500, height=2500)
-        fig.update_layout(title_text='<i><b>Histograms matrix</b></i>')
-        extra_data["histograms"] = context.log_artifact(
-            PlotlyArtifact(key="histograms", figure=fig),
-            local_path=f"{plots_dest}/hist.html",
-            db_key=False,
+        df_new = df.copy()
+        df_new[label_column] = df_new[label_column].apply(str)
+        fig = ff.create_scatterplotmatrix(
+            df_new, diag="histogram", index=label_column, width=2500, height=2500
+        )
+        fig.update_layout(title_text="<i><b>Histograms matrix</b></i>")
+        extra_data["histogram matrix"] = context.log_artifact(
+            PlotlyArtifact(key="histograms matrix", figure=fig),
+            local_path=f"{plots_dest}/hist_mat.html",
         )
     except Exception as e:
-        context.logger.error(f"Failed to create pairplot histograms due to: {e}")
-    df[label_column] = df[label_column].astype(np.float64)
+        context.logger.error(f"Failed to create histogram matrix artifact due to: {e}")
 
-    # violin
+
+def _create_features_histogram_artifacts(
+    context: MLClientCtx,
+    df: pd.DataFrame,
+    extra_data: dict,
+    label_column: str,
+    plots_dest: str,
+):
+    """
+    Create and log a histogram artifact for each feature
+    """
     try:
-        fig = go.Figure()
-        for (columnName, columnData) in df.iteritems():
-            fig.add_trace(go.Violin(x=[columnName]*columnData.shape[0],
-                                    y=columnData,
-                                    name=columnName,
-                                    box_visible=True,
-                                    meanline_visible=True))
+        for (columnName, _) in df.iteritems():
+            if columnName == label_column:
+                continue
+            fig = px.histogram(
+                df,
+                x=columnName,
+                color=label_column,
+                marginal="box",
+                hover_data=df.columns,
+            )
 
-        fig.update_layout(title_text='<i><b>Violin Plot</b></i>')
+            fig.update_layout(title_text=f"<i><b>Histograms {columnName}</b></i>")
+            extra_data[f"histogram_{columnName}"] = context.log_artifact(
+                PlotlyArtifact(key=f"histogram_{columnName}", figure=fig),
+                local_path=f"{plots_dest}/hist_{columnName}.html",
+            )
+    except Exception as e:
+        context.logger.error(f"Failed to create pairplot histograms due to: {e}")
+
+
+def _create_violin_artifact(
+    context: MLClientCtx, df: pd.DataFrame, extra_data: dict, plots_dest: str
+):
+    """
+    Create and log a violin artifact
+    """
+    try:
+        fig = make_subplots(rows=(df.shape[1] // 3) + 1, cols=3)
+        i = 0
+        for (columnName, columnData) in df.iteritems():
+            fig.add_trace(
+                go.Violin(
+                    x=[columnName] * columnData.shape[0],
+                    y=columnData,
+                    name=columnName,
+                    # box_visible=True,
+                    # meanline_visible=True
+                ),
+                row=(i // 3) + 1,
+                col=(i % 3) + 1,
+            )
+            i += 1
+
+        fig.update_layout(title_text="<i><b>Violin Plots</b></i>")
         extra_data["violin"] = context.log_artifact(
             PlotlyArtifact(key="violin", figure=fig),
             local_path=f"{plots_dest}/violin.html",
-            db_key=False,
         )
     except Exception as e:
         context.logger.warn(f"Failed to create violin distribution plots due to: {e}")
 
-    # imbalance
+
+def _create_imbalance_artifact(
+    context: MLClientCtx,
+    df: pd.DataFrame,
+    extra_data: dict,
+    label_column: str,
+    plots_dest: str,
+):
+    """
+    Create and log an imbalance class artifact (csv + plot)
+    """
     if label_column:
         labels_count = df[label_column].value_counts().sort_index()
         df_labels_count = pd.DataFrame(labels_count)
@@ -87,54 +191,55 @@ def summarize(
         df_labels_count[label_column] = labels_count.index
         try:
             fig = px.pie(df_labels_count, names=label_column, values="Total")
-            fig.update_layout(title_text='<i><b>Labels balance</b></i>')
+            fig.update_layout(title_text="<i><b>Labels balance</b></i>")
             extra_data["imbalance"] = context.log_artifact(
                 PlotlyArtifact(key="imbalance", figure=fig),
                 local_path=f"{plots_dest}/imbalance.html",
             )
         except Exception as e:
             context.logger.warn(f"Failed to create class imbalance plot due to: {e}")
-        context.log_artifact(
+        extra_data["imbalance csv"] = context.log_artifact(
             TableArtifact(
                 "imbalance-weights-vec", df=pd.DataFrame({"weights": labels_count})
             ),
             local_path=f"{plots_dest}/imbalance-weights-vec.csv",
-            db_key=False,
         )
 
-    # correlation - matrix
+
+def _create_corr_artifact(
+    context: MLClientCtx,
+    df: pd.DataFrame,
+    extra_data: dict,
+    label_column: str,
+    plots_dest: str,
+):
+    """
+    Create and log an correlation-matrix artifact (csv + plot)
+    """
     df.drop([label_column], axis=1, inplace=True)
     tblcorr = df.corr()
-    context.log_artifact(
-        TableArtifact("correlation-matrix", df=tblcorr, visible=True),
+    extra_data["correlation-matrix csv"] = context.log_artifact(
+        TableArtifact("correlation-matrix csv", df=tblcorr, visible=True),
         local_path=f"{plots_dest}/correlation-matrix.csv",
-        db_key=False,
     )
 
     try:
         z = tblcorr.values.tolist()
         z_text = [["{:.2f}".format(y) for y in x] for x in z]
-        fig = ff.create_annotated_heatmap(z, x=list(df.columns),
-                                          y=list(df.columns),
-                                          annotation_text=z_text, colorscale='agsunset')
-        fig['layout']['yaxis']['autorange'] = "reversed"  # l -> r
-        fig.update_layout(title_text='<i><b>Correlation matrix</b></i>')
-        fig['data'][0]['showscale'] = True
+        fig = ff.create_annotated_heatmap(
+            z,
+            x=list(df.columns),
+            y=list(df.columns),
+            annotation_text=z_text,
+            colorscale="agsunset",
+        )
+        fig["layout"]["yaxis"]["autorange"] = "reversed"  # l -> r
+        fig.update_layout(title_text="<i><b>Correlation matrix</b></i>")
+        fig["data"][0]["showscale"] = True
 
-        extra_data["correlation"] = context.log_artifact(
-            PlotlyArtifact(key="correlation", figure=fig),
+        extra_data["correlation-matrix"] = context.log_artifact(
+            PlotlyArtifact(key="correlation-matrix", figure=fig),
             local_path=f"{plots_dest}/corr.html",
-            db_key=False,
         )
     except Exception as e:
         context.logger.warn(f"Failed to create features correlation plot due to: {e}")
-    if update_dataset and table.meta and table.meta.kind == "dataset":
-        from mlrun.artifacts import update_dataset_meta
-
-        update_dataset_meta(table.meta, extra_data=extra_data)
-
-    # TODO : Plots according to client wishes - like preform histogram on selected features.
-    # TODO : 3-D plot on on selected features.
-    # TODO : Reintegration plot on on selected features.
-
-
--- describe/test_describe.py
+++ describe/test_describe.py
@@ -1,41 +1,38 @@
 import os
 import shutil
 from pathlib import Path
-
-import pytest
-from mlrun import import_function
-from sklearn.datasets import make_classification
-import numpy as np
-import pandas as pd
-import mlrun
-
-from describe import summarize
 from typing import Set
 
-DATA_URL = "https://s3.wasabisys.com/iguazio/data/iris/iris_dataset.csv"
+import mlrun
+import pandas as pd
+import pytest
+from mlrun import import_function
+from mlrun.execution import MLClientCtx
+from sklearn.datasets import make_classification
+
+DATA_PATH = os.path.abspath("./artifacts/random_dataset.parquet")
 PLOTS_PATH = os.path.abspath("./artifacts/plots")
 ARTIFACTS_PATH = os.path.abspath("./artifacts")
 
 
-PARAM = [{"name": "small_data", "n_samples": 100, "n_features": 5, "n_classes": 3, "n_informative": 3},
-         {"name": "big_amount_of_samples", "n_samples": 10000, "n_features": 5, "n_classes": 3, "n_informative": 3},
-         {"name": "big_amount_of_features", "n_samples": 100, "n_features": 20, "n_classes": 3, "n_informative": 3},
-         {"name": "big_data", "n_samples": 10000, "n_features": 20, "n_classes": 4, "n_informative": 5},
-         ]
-
-
 def _validate_paths(paths: Set):
+    """
+    Check if all the expected plot are saved
+    """
     base_folder = PLOTS_PATH
     for path in paths:
         full_path = os.path.join(base_folder, path)
         if Path(full_path).is_file():
-            print("File exist")
+            print(f"{path} exist")
         else:
-            raise FileNotFoundError
+            assert FileNotFoundError(f"{path} not found!")
 
 
 @pytest.fixture(autouse=True)
 def run_around_tests():
+    """
+    Wraps the tests and resets the local paths + activate _validate_paths after each test
+    """
     if Path(PLOTS_PATH).is_dir():
         shutil.rmtree(PLOTS_PATH)
     if Path(ARTIFACTS_PATH).is_dir():
@@ -46,7 +43,7 @@ def run_around_tests():
         {
             "corr.html",
             "correlation-matrix.csv",
-            "hist.html",
+            "hist_mat.html",
             "imbalance.html",
             "imbalance-weights-vec.csv",
             "violin.html",
@@ -55,43 +52,134 @@ def run_around_tests():
 
 
 def test_sanity_local():
-    # Setting environment:
+    """
+    Test simple scenario
+    """
     describe_func = import_function("function.yaml")
-
+    is_test_passed = True
+    _creat_data(n_samples=100, n_features=5, n_classes=3, n_informative=3)
     try:
         describe_run = describe_func.run(
             name="task-describe",
-            handler='summarize',
-            inputs={"table": DATA_URL},
+            handler="analysis",
+            inputs={"table": DATA_PATH},
             params={"update_dataset": True, "label_column": "label"},
             artifact_path=os.path.abspath("./artifacts"),
-            local=True
+            local=True,
         )
 
     except Exception as exception:
         print(f"- The test failed - raised the following error:\n- {exception}")
+        is_test_passed = False
+    assert is_test_passed
 
 
-@pytest.mark.parametrize("param", PARAM)
-def test_different_size_of_dataset(param):
-    X, y = make_classification(n_samples=param['n_samples'], n_features=param['n_features'],
-                               n_classes=param['n_classes'], n_informative=param['n_informative'],
-                               random_state=18, class_sep=2,)
-    df = pd.DataFrame(X, columns=[f"feature_{i}" for i in range(param['n_features'])])
-    df['label'] = y
-    data_path = "artifacts/random_dataset.parquet"
-    df.to_parquet("artifacts/random_dataset.parquet")
-
+@pytest.mark.parametrize("n_samples", [100, 1000, 5000, 10000])
+@pytest.mark.parametrize("n_features", [5, 10, 20])
+@pytest.mark.parametrize("n_classes", [3, 4])
+@pytest.mark.parametrize("n_informative", [3])
+def test_different_size_of_dataset(n_samples, n_features, n_classes, n_informative):
+    """
+    Test different size of data
+    """
+    is_test_passed = True
+    df = _creat_data(n_samples, n_features, n_classes, n_informative)
     describe_func = import_function("function.yaml")
 
     try:
         describe_run = describe_func.run(
             name="task-describe",
-            handler='summarize',
-            inputs={"table": data_path},
+            handler="analysis",
+            inputs={"table": DATA_PATH},
             params={"update_dataset": True, "label_column": "label"},
             artifact_path=os.path.abspath("./artifacts"),
-            local=True
+            local=True,
         )
     except Exception as exception:
-        print(f"- {param['name']} test failed - raised the following error:\n- {exception}")
+        print(f" The test failed - raised the following error:\n- {exception}")
+        is_test_passed = False
+
+    _validate_paths({f"hist_{col}.html" for col in df.columns if col is not "label"})
+    assert is_test_passed
+
+
+def test_data_already_loaded():
+    """
+    Test scenario on already loaded data artifact
+    """
+
+    log_data_function = mlrun.code_to_function(
+        filename="test_describe.py",
+        name="log_data",
+        kind="job",
+        image="mlrun/ml-models",
+    )
+    df = _creat_data(n_samples=100, n_features=5, n_classes=3, n_informative=3)
+    log_data_run = log_data_function.run(
+        handler="_log_data",
+        params={"table": DATA_PATH},
+        local=True,
+    )
+    describe_func = import_function("function.yaml")
+    is_test_passed = True
+    try:
+        describe_run = describe_func.run(
+            name="task-describe",
+            handler="analysis",
+            inputs={"table": log_data_run.outputs["dataset"]},
+            params={"update_dataset": True, "label_column": "label"},
+            artifact_path=os.path.abspath("./artifacts"),
+            local=True,
+        )
+
+    except Exception as exception:
+        print(f"- The test failed - raised the following error:\n- {exception}")
+        is_test_passed = False
+
+    created_artifact = [*describe_run.outputs.keys()]
+
+    expected_artifacts = [
+        f"histogram_{col}" for col in df.columns if col is not "label"
+    ] + [
+        "correlation-matrix csv",
+        "correlation-matrix",
+        "histograms matrix",
+        "imbalance",
+        "imbalance-weights-vec",
+        "violin",
+    ]
+    for artifact in expected_artifacts:
+        if artifact not in created_artifact:
+            print(f"{artifact} artifact not found!")
+            is_test_passed = False
+
+            break
+    assert is_test_passed
+
+
+def _log_data(context: MLClientCtx, table: str):
+    """
+    Log the data using context.log_dataset
+    """
+    df = pd.read_parquet(table)
+    context.log_dataset(key="dataset", db_key="dataset", stats=True, df=df)
+
+
+def _creat_data(n_samples, n_features, n_classes, n_informative):
+    """
+    Create df and save it as artifacts/random_dataset.parquet
+    """
+    X, y = make_classification(
+        n_samples=n_samples,
+        n_features=n_features,
+        n_classes=n_classes,
+        n_informative=n_informative,
+        n_redundant=0,
+        n_repeated=0,
+        random_state=18,
+        class_sep=2,
+    )
+    df = pd.DataFrame(X, columns=[f"feature_{i}" for i in range(n_features)])
+    df["label"] = y
+    df.to_parquet("artifacts/random_dataset.parquet")
+    return df

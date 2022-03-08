390aee878233857ede8f231578517048638886a2
brennanreese <brs3554@gmail.com>
2022-03-08 17:52:32 -0500

    testing read_snowflake

---
+++ read_snowflake/read_snowflake.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+
+def read_snowflake(db, schema, warehouse, query, target):
+    """
+    Read Snowflake and write results to parquet
+    :param db: Snowflake database
+    :param schema: Snowflake schema
+    :param warehouse: Snowflake warehouse instance
+    :param query: Snowflake query string
+    :param target: Full path to parquet output eg: "/v3io/bigdata/output.parquet"
+    """
+    spark = SparkSession.builder.appName("read_snowflake").getOrCreate()
+
+    sfOptions = {
+      "sfURL" : "$sfURL",
+      "sfUser" : "$sfUser",
+      "sfPassword" : "$sfPassword",
+      "sfDatabase" : db,
+      "sfSchema" : schema,
+      "sfWarehouse" : warehouse
+    }
+
+    df = spark.read.format("net.snowflake.spark.snowflake") \
+      .options(**sfOptions) \
+      .option("query",  query) \
+      .load()
+    
+    df.write.parquet(target)

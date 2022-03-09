374880a8193858a9b903a92cc9fe057c52839666
brennanreese <brs3554@gmail.com>
2022-03-09 15:48:21 -0500

    updates

--- read_snowflake/read_snowflake.py
+++ read_snowflake/read_snowflake.py
@@ -1,20 +1,24 @@
 from pyspark.sql import SparkSession
 
-def read_snowflake(db, schema, warehouse, query, target):
+def read_snowflake(config, db, schema, warehouse, query, target):
     """
     Read Snowflake and write results to parquet
     :param db: Snowflake database
     :param schema: Snowflake schema
     :param warehouse: Snowflake warehouse instance
     :param query: Snowflake query string
-    :param target: Full path to parquet output eg: "/v3io/bigdata/output.parquet"
+    :param target: Full path to parquet output eg: "v3io://bigdata/output.parquet"
     """
     spark = SparkSession.builder.appName("read_snowflake").getOrCreate()
 
+    sfURL = config["sfURL"]
+    sfUser = config["sfUser"]
+    sfPassword = config["sfPassword"]
+    
     sfOptions = {
-      "sfURL" : "$sfURL",
-      "sfUser" : "$sfUser",
-      "sfPassword" : "$sfPassword",
+      "sfURL" : sfURL,
+      "sfUser" : sfUser,
+      "sfPassword" : sfPassword,
       "sfDatabase" : db,
       "sfSchema" : schema,
       "sfWarehouse" : warehouse

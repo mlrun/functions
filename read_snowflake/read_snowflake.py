b1f8c1721a21ee3e21fca31ce4211592d68299af
brennanreese <brs3554@gmail.com>
2022-03-21 18:35:17 -0400

    Final changes

--- read_snowflake/read_snowflake.py
+++ read_snowflake/read_snowflake.py
@@ -1,19 +1,33 @@
+import os
+from mlrun.execution import MLClientCtx
 from pyspark.sql import SparkSession
 
-def read_snowflake(config, db, schema, warehouse, query, target):
+def read_snowflake(context: MLClientCtx, 
+                   url: str, 
+                   user: str, 
+                   password:str, 
+                   db:str, 
+                   schema:str, 
+                   warehouse:str, 
+                   query:str, 
+                   target:str) -> None:
     """
     Read Snowflake and write results to parquet
+    :param context: MLRun function context
+    :param url: Snowflake URL secret
+    :param user: Snowflake user secret
+    :param password: Snowflake user password secret
     :param db: Snowflake database
     :param schema: Snowflake schema
-    :param warehouse: Snowflake warehouse instance
-    :param query: Snowflake query string
+    :param warehouse: Snowflake warehouse instance eg: "compute_wh"
+    :param query: Snowflake query string eg: "select * from customer"
     :param target: Full path to parquet output eg: "v3io://bigdata/output.parquet"
     """
     spark = SparkSession.builder.appName("read_snowflake").getOrCreate()
 
-    sfURL = config["sfURL"]
-    sfUser = config["sfUser"]
-    sfPassword = config["sfPassword"]
+    sfURL = context.get_secret(url)
+    sfUser = context.get_secret(user)
+    sfPassword = context.get_secret(password)
     
     sfOptions = {
       "sfURL" : sfURL,
@@ -21,7 +35,8 @@ def read_snowflake(config, db, schema, warehouse, query, target):
       "sfPassword" : sfPassword,
       "sfDatabase" : db,
       "sfSchema" : schema,
-      "sfWarehouse" : warehouse
+      "sfWarehouse" : warehouse,
+      "application" : f"Iguazio-{os.getenv('SNOWFLAKE_APPLICATION', 'application')}"
     }
 
     df = spark.read.format("net.snowflake.spark.snowflake") \

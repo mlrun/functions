e95304f4aa7a3154a87c58553e6656f52063f254
Yonatan Shelach <yonatanshelach@Yonatans-MacBook-Pro.local>
2022-09-13 09:48:26 +0300

    fixed test of feature_perms

--- feature_perms/test_feature_perms.py
+++ feature_perms/test_feature_perms.py
@@ -18,55 +18,37 @@ import os
 
 ARTIFACTS_PATH = 'artifacts'
 DATA_URL = "https://raw.githubusercontent.com/parrt/random-forest-importances/master/notebooks/data/rent.csv"
-FEATURE_OUTPUT = ARTIFACTS_PATH + "/feature-importances-permute-tbl.parquet"
+FEATURE_OUTPUT = "feature-importances-permute-tbl"
 
 
 def arc_to_parquet():
     from mlrun import import_function
-    from mlrun.platforms import auto_mount
 
     archive_func = import_function('hub://arc_to_parquet')
-    archive_run = archive_func.run(handler="arc_to_parquet",
-                                   params={"key": "rent", "stats": True, "file_ext": "csv"},
-                                   inputs={"archive_url": DATA_URL},
-                                   artifact_path=os.getcwd() + '/artifacts'
-                                   , local=True
-                                   )
+
+    archive_run = archive_func.run(
+        handler="arc_to_parquet",
+        params={"key": "rent", "stats": True, "file_ext": "csv"},
+        inputs={"archive_url": DATA_URL},
+        artifact_path=os.getcwd() + '/artifacts',
+        local=True,
+    )
+
+    return archive_run.artifact('rent').url
 
 
 def sklearn_classifier(run):
     cwd = os.getcwd()
     file_path = str(Path(cwd).parent.absolute()) + "/sklearn_classifier/sklearn_classifier.py"
-    fn = code_to_function(name='test_sklearn_classifier',
-                          filename=file_path,
-                          handler="train_model",
-                          kind="local",
-                          )
-    fn.spec.command = file_path
-    fn.run(params={
-        "sample": -5_000,  # 5k random rows,
-        "model_pkg_class": "sklearn.ensemble.RandomForestClassifier",
-        "label_column": "interest_level",
-        "CLASS_n_estimators": 100,
-        "CLASS_min_samples_leaf": 1,
-        "CLASS_n_jobs": -1,
-        "CLASS_oob_score": True},
+    fn = code_to_function(
+        name='test_sklearn_classifier',
+        filename=file_path,
         handler="train_model",
-        inputs={"dataset": run.outputs["rent"]},
-        artifact_path='artifacts'
-        # , local=True
+        kind="local",
     )
 
-
-def train_model():
-    from mlrun import import_function
-    from mlrun.platforms import auto_mount
-
-    train = import_function('hub://sklearn_classifier')
-    # .apply(auto_mount())
-
-    train_run = train.run(
-        inputs={"dataset": "artifacts/rent.csv"},
+    fn.spec.command = file_path
+    fn.run(
         params={
             "sample": -5_000,  # 5k random rows,
             "model_pkg_class": "sklearn.ensemble.RandomForestClassifier",
@@ -74,39 +56,79 @@ def train_model():
             "CLASS_n_estimators": 100,
             "CLASS_min_samples_leaf": 1,
             "CLASS_n_jobs": -1,
-            "CLASS_oob_score": True},
-        local=True)
+            "CLASS_oob_score": True,
+        },
+        handler="train_model",
+        inputs={"dataset": run.outputs["rent"]},
+        artifact_path='artifacts',
+    )
+
+
+def train_model(data):
+    from mlrun import import_function
+
+    train = import_function('hub://sklearn_classifier')
+
+    train_run = train.run(
+        inputs={"dataset": data},
+        params={
+            "sample": -5_000,  # 5k random rows,
+            "model_pkg_class": "sklearn.ensemble.RandomForestClassifier",
+            "label_column": "interest_level",
+            "CLASS_n_estimators": 100,
+            "CLASS_min_samples_leaf": 1,
+            "CLASS_n_jobs": -1,
+            "CLASS_oob_score": True,
+        },
+        local=True
+    )
+
+    return train_run.artifact('model').url
 
 
 def test_feature_selection_run_local():
-    arc_to_parquet()
-    train_model()
-    data = "artifacts/rent.csv"
+    data = arc_to_parquet()
+    model = train_model(data)
     labels = "interest_level"
-    model = "model/model.pkl"
-    fn = code_to_function(name='test_run_local_feature_perms',
-                          filename="feature_perms.py",
-                          handler="permutation_importance",
-                          kind="local",
-                          )
+    fn = code_to_function(
+        name='test_run_local_feature_perms',
+        filename="feature_perms.py",
+        handler="permutation_importance",
+        kind="local",
+    )
     fn.spec.command = "feature_perms.py"
-    fi_perms = fn.run(params={"labels": labels,
-                              "plots_dest": "plots"},
-                      inputs={"model": model, "dataset": data},
-                      artifact_path='artifacts')
-    assert Path(FEATURE_OUTPUT).is_file()
+
+    run = fn.run(
+        params={
+            "labels": labels,
+            "plots_dest": "plots",
+        },
+        inputs={
+            "model": model,
+            "dataset": data,
+        },
+        artifact_path='artifacts',
+    )
+
+    assert run.artifact(FEATURE_OUTPUT).get()
 
 
 def test_feature_perms_import_function():
-    arc_to_parquet()
-    train_model()
-    data = "artifacts/rent.csv"
+    data = arc_to_parquet()
+    model = train_model(data)
     labels = "interest_level"
-    model = "model/model.pkl"
-    fi_perms = import_function("function.yaml")
-    fi_perms.run(params={"labels": labels,
-                         "plots_dest": "plots"},
-                 inputs={"model": model, "dataset": data},
-                 artifact_path=os.getcwd() + '/artifacts'
-                 , local=True)
-    assert Path(FEATURE_OUTPUT).is_file()
+    fn = import_function("function.yaml")
+
+    run = fn.run(
+        params={
+            "labels": labels,
+            "plots_dest": "plots"
+        },
+        inputs={
+            "model": model,
+            "dataset": data},
+        artifact_path=os.getcwd() + '/artifacts',
+        local=True,
+    )
+
+    assert run.artifact(FEATURE_OUTPUT).get()

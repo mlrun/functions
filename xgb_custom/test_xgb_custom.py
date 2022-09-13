6737d48870fef459ca50254b9ba3d6e9d33bc2ca
Yonatan Shelach <yonatanshelach@Yonatans-MacBook-Pro.local>
2022-09-13 11:22:10 +0300

    fixed test of xgb_custom

--- xgb_custom/test_xgb_custom.py
+++ xgb_custom/test_xgb_custom.py
@@ -25,22 +25,26 @@ SCHEDULES_PATH = "schedules"
 
 def test_local_xgb_custom():
     fn = import_function("function.yaml")
-    fn.run(params={"nrows": 8192,
-                   "label_type": "float",
-                   "local_path": "./artifacts/inputs/xgb_custom"},
-           handler="gen_outliers",
-           local=True)
+    run = fn.run(
+        params={
+            "nrows": 8192,
+            "label_type": "float",
+            "local_path": "./artifacts/inputs/xgb_custom",
+        },
+        handler="gen_outliers",
+        local=True,
+    )
 
-    fn.run(params={"num_boost_round": 40,
-                   "verbose_eval": False,
-                   "XGB_max_depth": 2,
-                   "XGB_subsample": 0.9,
-                   "test_set_key": "./artifacts/inputs/test-set"},
-           inputs={"dataset": "./artifacts/inputs/xgb_custom.parquet"},
-           handler="fit",
-           local=True)
-
-    assert(os.path.exists(os.getcwd() + "/plots/learning-curves.html"))
-
-
-
+    run = fn.run(
+        params={
+            "num_boost_round": 40,
+            "verbose_eval": False,
+            "XGB_max_depth": 2,
+            "XGB_subsample": 0.9,
+            "test_set_key": "./artifacts/inputs/test-set",
+        },
+        inputs={"dataset": run.artifact('xgb-outs').url},
+        handler="fit",
+        local=True,
+    )
+    assert run.artifact('learning-curves').get()

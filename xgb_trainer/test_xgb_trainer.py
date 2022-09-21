cc764f9ce55ed6c627b8e43319b0566289c23b27
daniels290813 <daniels@iguazio.com>
2022-09-21 07:35:21 +0000

    [xgb_trainer] dynamic artifact url

--- xgb_trainer/test_xgb_trainer.py
+++ xgb_trainer/test_xgb_trainer.py
@@ -1,95 +1,67 @@
 # Copyright 2019 Iguazio
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the “License”);
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an “AS IS” BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
 from mlrun import code_to_function, import_function, DataItem
+from mlrun.execution import MLClientCtx
 import os
 
 
-def get_class_data():
-    fn = import_function("hub://gen_class_data")
-    fn.run(params={
-        "n_samples": 10_000,
-        "m_features": 5,
-        "k_classes": 2,
-        "header": None,
-        "weight": [0.5, 0.5],
-        "sk_params": {"n_informative": 2},
-        "file_ext": "csv"}, local=True, artifact_path="./artifacts/inputs")
+def get_class_data(context: MLClientCtx):
+    fn = import_function('hub://gen_class_data')
+    run = fn.run(params={'context': context,
+                         'key': 'classifier-data',
+                         'n_samples': 10_000,
+                         'm_features': 5,
+                         'k_classes': 2,
+                         'header': None,
+                         'weight': [0.5, 0.5],
+                         'sk_params': {'n_informative': 2},
+                         'file_ext': 'csv'}, local=True, artifact_path='./')
+    return run
 
-
-def test_xgb_trainer_code_to_function():
-    get_class_data()
+def test_xgb_trainer_code_to_function(context: MLClientCtx):
+    gen_data_run = get_class_data(context)
     fn = code_to_function(name='test_xgb_trainer',
-                          filename="xgb_trainer.py",
-                          handler="train_model",
-                          kind="local",
-                          )
-    fn.spec.command = "xgb_trainer.py"
-    fn.run(params={
-        "model_type": "classifier",
-        "CLASS_tree_method": "hist",
-        "CLASS_objective": "binary:logistic",
-        "CLASS_booster": "gbtree",
-        "FIT_verbose": 0,
-        "label_column": "labels",
-        "test_set": "./artifacts/test-set"},
-        # local=True,
-        inputs={"dataset": './artifacts/inputs/classifier-data.csv'})
+                          filename='xgb_trainer.py',
+                          handler='train_model',
+                          kind='local')
+    
+    run = fn.run(params={'model_type': 'classifier',
+                         'CLASS_tree_method': 'hist',
+                         'CLASS_objective': 'binary:logistic',
+                         'CLASS_booster': 'gbtree',
+                         'FIT_verbose': 0,
+                         'label_column': 'labels',
+                         'test_set': './'},
+                 local=False,
+                 inputs={'dataset': gen_data_run.artifact('classifier-data').url})
 
-    assert (os.path.exists(os.getcwd() + "/models/model.pkl"))
+    assert (run.artifact('model'))
 
 
-def test_local_xgb_trainer_import_function():
+def test_local_xgb_trainer_import_function(context: MLClientCtx):
     # importing data preparation function locally
-    get_class_data()
+    gen_data_run = get_class_data(context)
 
-    fn = import_function("function.yaml")
-    fn.run(params={
-        "model_type": "classifier",
-        "CLASS_tree_method": "hist",
-        "CLASS_objective": "binary:logistic",
-        "CLASS_booster": "gbtree",
-        "FIT_verbose": 0,
-        "label_column": "labels",
-        "test_set": "./artifacts/test-set"},
-        local=True, inputs={"dataset": './artifacts/inputs/classifier-data.csv'})
+    fn = import_function('function.yaml')
+    run = fn.run(params={'model_type': 'classifier',
+                         'CLASS_tree_method': 'hist',
+                         'CLASS_objective': 'binary:logistic',
+                         'CLASS_booster': 'gbtree',
+                         'FIT_verbose': 0,
+                         'label_column': 'labels',
+                         'test_set': './'},
+                 local=True, inputs={'dataset': gen_data_run.artifact('classifier-data').url})
 
-    assert (os.path.exists(os.getcwd() + "/models/model.pkl"))
-
-
-# def test_xgb_trainer_regression_code_to_function():
-#     from sklearn.datasets import load_boston
-#     boston = load_boston()
-#     import pandas as pd
-#     data = pd.DataFrame(boston.data)
-#     data.columns = boston.feature_names
-#     data['PRICE'] = boston.target
-#     data.to_csv("artifacts/boston.csv")
-#
-#     fn = code_to_function(name='test_xgb_trainer',
-#                           filename="xgb_trainer.py",
-#                           handler="train_model",
-#                           kind="local",
-#                           )
-#     fn.run(params={
-#         "model_type": "regressor",
-#         "CLASS_objective": "reg:linear",
-#         "FIT_verbose": 0,
-#         "label_column": "PRICE",
-#         "test_set": "./artifacts/test-set"},
-#         inputs={"dataset": 'artifacts/boston.csv'},
-#         local=True
-#     )
-#
-#     assert (os.path.exists(os.getcwd() + "/models/model.pkl"))
+    assert (run.artifact('model'))

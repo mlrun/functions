02f615908da04187e251a27f842e98e85871f36f
peng wei <pengwei715@gmail.com>
2023-07-20 11:50:16 -0700

    adding the licence infor at the very beginning

--- pii_recognizer/pii_recognizer.py
+++ pii_recognizer/pii_recognizer.py
@@ -1,3 +1,18 @@
+# Copyright 2019 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import mlrun
 from mlrun.datastore import DataItem
 from mlrun.artifacts import Artifact
@@ -9,6 +24,7 @@ import warnings
 import os
 import logging
 from typing import Optional, List, Tuple, Set
+import presidio_anonymizer as pa
 
 from presidio_analyzer import (
     RecognizerRegistry,

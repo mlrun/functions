63fa485600797edb8020f86b20a56e20e2928946
Yonatan Shelach <92271540+yonishelach@users.noreply.github.com>
2023-08-16 19:07:34 +0300

    [Transcribe] Run test only with python 3.8 at least (#645)
    * [Transcribe] Run test only with python 3.8 at least
    
    * removed import before skip

--- transcribe/test_transcribe.py
+++ transcribe/test_transcribe.py
@@ -14,12 +14,12 @@
 #
 import os
 import pathlib
+import sys
 import tempfile
 from difflib import SequenceMatcher
 
 import mlrun
 import pytest
-import whisper
 
 expected_outputs = [
     "This is a speech to text test.",
@@ -29,9 +29,13 @@ expected_outputs = [
     "The crowd roars, a symphony of passion, "
     "as the game writes its unpredictable story on the field of destiny.",
 ]
+whisper_models = ["tiny_en", "tiny", "base_en", "base_en"]
 
-
-@pytest.mark.parametrize("model_name", whisper.available_models()[:4])
+@pytest.mark.skipif(
+    condition=sys.version_info[:2] < (3, 8),
+    reason="whisper requires python 3.8 and above"
+)
+@pytest.mark.parametrize("model_name", whisper_models)
 @pytest.mark.parametrize("audio_path", ["./data", "./data/speech_01.mp3"])
 def test_transcribe(model_name: str, audio_path: str):
     # Setting variables and importing function:

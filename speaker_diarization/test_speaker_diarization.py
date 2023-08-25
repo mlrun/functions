1d71b654e52f831b68d38351a5e92ef994f13be0
peng wei <pengwei715@gmail.com>
2023-08-24 17:37:03 -0700

    testing

--- speaker_diarization/test_speaker_diarization.py
+++ speaker_diarization/test_speaker_diarization.py
@@ -86,9 +86,7 @@ def test_convert_rttm_to_annotation_df():
     # Create a temporary file and write the sample RTTM content to it
     with tempfile.NamedTemporaryFile(suffix=".rttm", mode='w+', delete=False) as temp_rttm_file:
         temp_rttm_file.write(sample_rttm_content)
-        temp_rttm_file_path = "".join(temp_rttm_file.name.split('/')[:-1])
-    import pdb
-    pdb.set_trace()
+        temp_rttm_file_path = os.path.dirname(temp_rttm_file.name)
     # Call the function to convert RTTM to annotation DataFrame
     annotation_df, _ = _convert_rttm_to_annotation_df(temp_rttm_file_path)
 

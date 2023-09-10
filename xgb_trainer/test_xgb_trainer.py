5e86bc56c69953509743c25c2dd857b5de1ac920
daniels290813 <78727943+daniels290813@users.noreply.github.com>
2023-09-10 13:51:05 +0300

    Update test_xgb_trainer.py

--- xgb_trainer/test_xgb_trainer.py
+++ xgb_trainer/test_xgb_trainer.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import mlrun
 from mlrun import code_to_function, import_function, DataItem
 from mlrun.execution import MLClientCtx
 import os

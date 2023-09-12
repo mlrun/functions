3693f45737766dc16b230930e8e4a13307e828b8
daniels290813 <daniels@iguazio.com>
2023-09-12 12:07:15 +0300

    [xgb-trainer] update test

--- xgb_trainer/test_xgb_trainer.py
+++ xgb_trainer/test_xgb_trainer.py
@@ -12,13 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from mlrun import code_to_function, import_function, DataItem
-from mlrun.execution import MLClientCtx
+import mlrun
 import os
 
 
 def get_class_data():
-    fn = import_function('hub://gen_class_data')
+    fn = mlrun.import_function('../gen_class_data/function.yaml')
     run = fn.run(params={'key': 'classifier-data',
                          'n_samples': 10_000,
                          'm_features': 5,
@@ -26,40 +25,25 @@ def get_class_data():
                          'header': None,
                          'weight': [0.5, 0.5],
                          'sk_params': {'n_informative': 2},
-                         'file_ext': 'csv'}, local=True, artifact_path='./')
+                         'file_ext': 'csv'}, local=True, artifact_path="./artifacts")
     return run
 
 
-def test_xgb_trainer_code_to_function():
-    gen_data_run = get_class_data()
-    fn = code_to_function(name='test_xgb_trainer',
-                          filename='xgb_trainer.py',
-                          handler='train_model',
-                          kind='local')
-    
-    run = fn.run(params={'model_type': 'classifier',
-                         'CLASS_tree_method': 'hist',
-                         'CLASS_objective': 'binary:logistic',
-                         'CLASS_booster': 'gbtree',
-                         'FIT_verbose': 0,
-                         'label_column': 'labels'},
-                 local=False,
-                 inputs={'dataset': gen_data_run.artifact('classifier-data').url})
-
-    assert (run.artifact('model'))
-
-
 def test_local_xgb_trainer_import_function():
-    # importing data preparation function locally
+    # running data preparation function locally
     gen_data_run = get_class_data()
 
-    fn = import_function('function.yaml')
+    fn = mlrun.import_function('function.yaml')
     run = fn.run(params={'model_type': 'classifier',
                          'CLASS_tree_method': 'hist',
                          'CLASS_objective': 'binary:logistic',
                          'CLASS_booster': 'gbtree',
                          'FIT_verbose': 0,
                          'label_column': 'labels'},
-                 local=True, inputs={'dataset': gen_data_run.artifact('classifier-data').url})
+                 local=True, inputs={'dataset': gen_data_run.status.artifacts[0]['spec']['target_path']})  # only one dataset artifact created
 
-    assert (run.artifact('model'))
+    for artifact in run.status.artifacts:
+        if artifact['kind'] == 'model':
+            assert os.path.exists(artifact['spec']['target_path'])  # validating model exists
+            return
+    assert False, "Model artifact is unavailable or miss-predicted"

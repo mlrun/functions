5f266787e85a9f48cd59b949fe02e95d8808a386
danielperezz <daniel06715@gmail.com>
2025-08-13 16:57:20 +0300

    remove the dummy module

--- modules/src/dummy_module/dummy_module.py
+++
@@ -1,7 +0,0 @@
-
-def dummy_module():
-    """
-    This is a dummy module that does nothing.
-    It is used to test the module system.
-    """
-    print("This is a dummy module that does nothing.")
--- modules/src/dummy_module/test_dummy_module.py
+++
@@ -1,6 +0,0 @@
-
-def test_dummy_module():
-
-    assert True, "Dummy module test failed"
-
-    print("Dummy module tests passed successfully.")
